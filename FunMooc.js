e4d05bdca5cd10490ee8cd07d9c683b32c9282b8
Abdoul54 <abdelwahed.akhechane@gmail.com>
2024-03-13 11:22:26 +0000

    Add Fun-Mooc course scraping endpoint

--- FunMooc.js
+++ FunMooc.js
@@ -1,20 +1,38 @@
 const puppeteer = require("puppeteer");
 
+/**
+ * Class to scrape course data from FunMooc
+ * @class
+ * @classdesc Class to scrape course data from FunMooc
+ * @returns {Object}
+ */
 class FunMoocScraper {
+  /**
+   * @constructor
+   * @returns {Object}
+   * @description Constructor for FunMoocScraper class
+   */
   constructor() {
-    this.openClassrooms = {
-      name: "//h1[@class='subheader__title']", //* Good
-      orga: "//a/meta[@property='name']", //* Good
+    this.funMooc = {
+      name: "//h1[@class='subheader__title']",
+      orga: "//a/meta[@property='name']",
       brief:
         "//*[@id='site-content']/div[2]/div[1]/div/div[1]/div[1]/div/div/p",
       programme:
-        "//*[@id='site-content']/div[2]/div[1]/div/div[1]/div[2]/section/div/ul/li/div/button",
+        "//div[@class='nested-item nested-item--accordion nested-item--1']",
       animateur: "//h3[@class='person-glimpse__title']",
-      languages:
-        "//*[@id='site-content']/div[1]/div[2]/div/div[1]/div[3]/div[2]/ul/div/li/span",
+      languages: "//div[@class='subheader__content']/div[2]/ul/div/li/span",
     };
   }
 
+  /**
+   * Extracts text from a given xpath
+   * @param {Object} page
+   * @param {String} xpath
+   * @returns {String}
+   * @description Extracts text from a given xpath
+   * @async
+   */
   async extractText(page, xpath) {
     return await page.evaluate((xpath) => {
       const element = document.evaluate(
@@ -28,6 +46,14 @@ class FunMoocScraper {
     }, xpath);
   }
 
+  /**
+   * Extracts text from a given xpath
+   * @param {Object} page
+   * @param {String} xpath
+   * @returns {String}
+   * @description Extracts text from a given xpath
+   * @async
+   */
   async extractMany(page, xpath) {
     return await page.evaluate((xpath) => {
       const iterator = document.evaluate(
@@ -47,6 +73,15 @@ class FunMoocScraper {
     }, xpath);
   }
 
+  /**
+   * Extracts attribute from all elements matching a given xpath
+   * @param {Object} page
+   * @param {String} xpath
+   * @param {String} attributeName
+   * @returns {Array}
+   * @description Extracts attribute from all elements matching a given xpath
+   * @async
+   */
   async extractAttributeFromAll(page, xpath, attributeName) {
     return await page.evaluate(
       (xpath, attributeName) => {
@@ -72,6 +107,13 @@ class FunMoocScraper {
     );
   }
 
+  /**
+   * Checks if a URL exists
+   * @param {String} url
+   * @returns {Boolean}
+   * @description Checks if a URL exists
+   * @async
+   */
   async checkURLExists(url) {
     try {
       const parsedURL = new URL(url);
@@ -87,6 +129,94 @@ class FunMoocScraper {
     }
   }
 
+  /**
+   * Extracts languages from a given string
+   * @param {String} data
+   * @returns {Array}
+   * @description Extracts languages from a given string
+   */
+  languageExtractor(data) {
+    let languages = [];
+    data = data
+      .toLowerCase()
+      .replace(",", "")
+      .split(":")[1]
+      .trim()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    // Check for each language and push the corresponding code into the array
+    if (data.includes("Français") || data.includes("French")) {
+      languages.push("fr");
+    }
+    if (data.includes("Anglais") || data.includes("English")) {
+      languages.push("en");
+    }
+    if (data.includes("Arabe") || data.includes("Arabic")) {
+      languages.push("ar");
+    }
+    return languages;
+  }
+
+  /**
+   * Extracts text from two xpaths and combines them
+   * @param {Object} page
+   * @param {String} xpath1
+   * @param {String} xpath2
+   * @returns {Array}
+   * @description Extracts text from two xpaths and combines them
+   * @async
+   */
+  async doubleExtraction(page, xpath1, xpath2) {
+    const texts = await page.evaluate(
+      (xpath1, xpath2) => {
+        const iterator1 = document.evaluate(
+          xpath1,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+          null
+        );
+
+        const iterator2 = document.evaluate(
+          xpath2,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+          null
+        );
+
+        let element1 = iterator1.iterateNext();
+        let element2 = iterator2.iterateNext();
+
+        const texts = [];
+
+        while (element1 && element2) {
+          const text1 = element1.textContent.trim();
+          const text2 = element2.textContent.trim();
+
+          const combinedText = `${text1} - ${text2}`;
+          texts.push(combinedText);
+
+          element1 = iterator1.iterateNext();
+          element2 = iterator2.iterateNext();
+        }
+
+        return texts;
+      },
+      xpath1,
+      xpath2
+    );
+
+    return texts;
+  }
+
+  /**
+   * Scrapes course data from a given URL
+   * @param {String} url
+   * @returns {Object}
+   * @description Scrapes course data from a given URL
+   * @async
+   */
   async scrapeCourseData(url) {
     let browser;
     let languages;
@@ -99,18 +229,33 @@ class FunMoocScraper {
       const page = await browser.newPage();
       await page.goto(url);
 
-      const [title, orga, brief, animateur, programme] = await Promise.all([
-        this.extractText(page, this.openClassrooms.name),
-        this.extractAttributeFromAll(page, this.openClassrooms.orga, "content"),
-        this.extractMany(page, this.openClassrooms.brief).then((brief) =>
-          brief.join("")
-        ),
-        this.extractMany(page, this.openClassrooms.animateur).then(
-          (animateur) => animateur
-        ),
-        this.extractText(page, this.openClassrooms.programme),
-      ]);
-
+      const [title, orga, brief, animateur, programme, languages] =
+        await Promise.all([
+          this.extractText(page, this.funMooc.name),
+          this.extractAttributeFromAll(
+            page,
+            this.funMooc.orga,
+            "content"
+          ),
+          this.extractMany(page, this.funMooc.brief).then((brief) =>
+            brief.join("")
+          ),
+          this.extractMany(page, this.funMooc.animateur).then(
+            (animateur) => animateur
+          ),
+          this.doubleExtraction(
+            page,
+            this.funMooc.programme,
+            this.funMooc.programme + "/ul/li/div/div"
+          ).then((programme) =>
+            programme.map((item) => {
+              return item.replace(/\n\s+/g, " ").trim();
+            })
+          ),
+          this.extractText(page, this.funMooc.languages).then((data) =>
+            this.languageExtractor(data)
+          ),
+        ]);
       return {
         title,
         url,
@@ -131,19 +276,21 @@ class FunMoocScraper {
   }
 }
 
-let scraper = new FunMoocScraper();
+module.exports = FunMoocScraper;
 
-scraper
-  .scrapeCourseData(
-    "https://www.fun-mooc.fr/fr/cours/orisat-teledetection-des-risques-naturels/"
-  )
-  .then((data) => console.log(data));
+// let scraper = new FunMoocScraper();
 
-scraper
-  .scrapeCourseData("https://www.fun-mooc.fr/fr/cours/medical-mycology/")
-  .then((data) => console.log(data));
+// scraper
+//   .scrapeCourseData(
+//     "https://www.fun-mooc.fr/fr/cours/orisat-teledetection-des-risques-naturels/"
+//   )
+//   .then((data) => console.log(data));
 
-//	!  buttons    //section/div/ul/li/div/button
-//	!  subbuttons //section/div/ul/li/div/ul/li/div/button
-// *[@id="site-content"]/div[2]/div[2]/div[1]/section/div/div[2]/div/div/a/h3
-// https://www.fun-mooc.fr/fr/cours/medical-mycology/
+// scraper
+//   .scrapeCourseData("https://www.fun-mooc.fr/fr/cours/medical-mycology/")
+//   .then((data) => console.log(data));
+
+// //	!  buttons    //section/div/ul/li/div/button
+// //	!  subbuttons //section/div/ul/li/div/ul/li/div/button
+// // *[@id="site-content"]/div[2]/div[2]/div[1]/section/div/div[2]/div/div/a/h3
+// // https://www.fun-mooc.fr/fr/cours/medical-mycology/
--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const Coursera = require("./coursera");
 const OpenClassrooms = require("./OpenClassroom");
 const cors = require("cors");
+const FunMooc = require("./FunMooc");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+
 /**
  * Scrape course data from Coursera
  * @param {string} url - URL of the course to scrape
  * @returns {Promise<object>} - Course data
  */
-
 app.post("/api/coursera/scrape-course", async (req, res) => {
   try {
     const { url } = req.body;
@@ -40,7 +41,6 @@ app.post("/api/coursera/scrape-course", async (req, res) => {
  * @param {string} url - URL of the course to scrape
  * @returns {Promise<object>} - Course data
  */
-
 app.post("/api/openclassrooms/scrape-course", async (req, res) => {
   try {
     const { url } = req.body;
@@ -62,6 +62,34 @@ app.post("/api/openclassrooms/scrape-course", async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 });
+
+/**
+ * Scrape course data from Fun-Mooc
+ * @param {string} url - URL of the course to scrape
+ * @returns {Promise<object>} - Course data
+ */
+app.post("/api/fun-mooc/scrape-course", async (req, res) => {
+  try {
+    const { url } = req.body;
+
+    if (!url) {
+      return res.status(400).json({ error: "URL parameter is required" });
+    }
+
+    const funMooc = new FunMooc();
+    const courseData = await funMooc.scrapeCourseData(url);
+
+    if (!courseData) {
+      return res.status(404).json({ error: "Course data not found" });
+    }
+
+    return res.json(courseData);
+  } catch (error) {
+    console.error("Error scraping course:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
