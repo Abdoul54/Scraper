7cfc82f762e0c8edf55b681a72b577d75581f8a2
Abdoul54 <abdelwahed.akhechane@gmail.com>
2024-03-22 11:42:50 +0000

    Added new files to .gitignore and updated EDX scraper

--- EdxScraper.js
+++ EdxScraper.js
@@ -1,122 +1,263 @@
 const Scraper = require("./Scraper");
+const langdetect = require("langdetect");
 /**
  * EDX scraper
  * @extends Scraper
  * @class
  */
 class EDX extends Scraper {
-  /**
-   * Create an EDX scraper
-   * @constructor
-   * @memberof EDX
-   * @method
-   */
-  constructor() {
-    super("EDX");
-    this.selectors = {
-      name: '//div[@class="course-about desktop course-info-content"]//div[1]/h1',
-      orga: '//div[@class="course-about desktop course-info-content"]//div[1]/ul/li[1]/a',
-      brief: '//div[@class="mt-2 lead-sm html-data"]',
-      programme: '//div[@class="mt-2 html-data"]/ul/li',
-      duration:
-        '//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]',
-      pace: '//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]',
-      animateur: '//div[@class="instructor-card px-4 py-3.5 rounded"]/div/h3',
-      languages:
-        '//div[@class="course-about desktop course-info-content"]/div[4]/div/div[2]/div/div/div[2]/ul/li[1]',
-    };
-    this.type = "course";
-  }
+	/**
+	 * Create an EDX scraper
+	 * @constructor
+	 * @memberof EDX
+	 * @method
+	 */
+	constructor() {
+		super("EDX");
+		this.selectors = {
+			name: '//div[@class="course-about desktop course-info-content"]//div[1]/h1',
+			orga: '//div[@class="course-about desktop course-info-content"]//div[1]/ul/li[1]/a',
+			brief: '//div[@class="mt-2 lead-sm html-data"]',
+			programme: '//div[@class="mt-2 html-data"]/ul/li',
+			altProgramme: '//div[@class="mt-2 html-data"]/p[1]',
+			altProgramme2: '//div[@class="mt-2 html-data"]/ol/li',
+			duration:
+				'//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]',
+			pace: '//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]',
+			animateur:
+				'//div[@class="instructor-card px-4 py-3.5 rounded"]/div/h3',
+			languages:
+				'//div[@class="course-about desktop course-info-content"]/div[4]/div/div[2]/div/div/div[2]/ul/li[1]',
+		};
+		this.type = "course";
+	}
 
-  /**
-   * Extract the languages and organization from the data
-   * @param {object} page - The Puppeteer page object
-   * @returns {object} - The extracted languages and organization
-   * @memberof EDX
-   * @method
-   * @async
-   */
-  async extractLanguagesAndOrga(page) {
-    try {
-      let elements = await page.$$(
-        'xpath///div[@class="course-about desktop course-info-content"]/div[4]/div//ul/li'
-      );
+	/**
+	 * Check the type of the URL
+	 * @param {string} url - The URL of the EDX course
+	 * @memberof EDX
+	 * @method
+	 */
+	checkType(url) {
+		if (url.includes("certificates")) {
+			this.type = "certificate";
+		}
+		this.switchSelectors();
+	}
 
-      let data = {};
-      for (let element of elements) {
-        let value = await element.evaluate((el) => el.textContent);
-        if (value.includes("Languages:") || value.includes("Language:")) {
-          data["languages"] = value.split(": ")[1];
-        }
-        if (value.includes("Institution:")) {
-          data["orga"] = value.split(": ")[1];
-        }
-      }
-      return data;
-    } catch (error) {
-      console.error("Error scraping course data:", error);
-      return null;
-    }
-  }
+	/**
+	 * Switch the selectors based on the type of the course
+	 * @memberof EDX
+	 * @method
+	 */
+	switchSelectors() {
+		if (this.type === "certificate") {
+			this.selectors.name = '//div[@class="title"]';
+			this.selectors.orga = '//div[@class="institution"]';
+			this.selectors.brief = '//div[@class="overview-info"]/p';
+			this.selectors.programme = '//li[@class="bullet-point mb-2"]';
+			this.selectors.duration =
+				'//div[@class="program-stat"][3]/div[@class="details"]/div[@class="main"]';
+			this.selectors.pace =
+				'//div[@class="program-stat"][3]/div[@class="details"]/div[@class="secondary"]';
+			this.selectors.animateur = '//a[@class="name font-weight-bold"]';
+		}
+	}
 
-  /**
-   * Scrape the data from the URL
-   * @param {string} url - The URL to scrape
-   * @returns {object} - The scraped data
-   * @memberof EDX
-   * @method
-   * @async
-   */
-  async scrape(url) {
-    try {
-      if (!(await super.checkURLExists(url))) {
-        console.error("URL '" + url + "' does not exist");
-        return;
-      }
-      var { browser, page } = await super.launchBrowser(url);
+	/**
+	 * Detect the language of the text
+	 * @param {string} text - The text to detect the language of it
+	 * @returns {array} - The detected languages
+	 * @memberof EDX
+	 * @method
+	 */
+	detectLanguage(text) {
+		if (langdetect.detect(text)[0].lang === "fr") {
+			return ["french"];
+		}
+		if (langdetect.detect(text)[0].lang === "en") {
+			return ["english"];
+		}
+		if (langdetect.detect(text)[0].lang === "ar") {
+			return ["arabic"];
+		}
+		return [];
+	}
 
-      const [title, brief, animateur, programme, duration, pace] =
-        await Promise.all([
-          super.extractText(page, this.selectors.name),
-          super
-            .extractMany(page, this.selectors.brief)
-            .then((brief) => brief.join(" ").trim().replace(/\n/g, "")),
-          super.extractManyWithMutation(page, this.selectors.animateur),
-          super
-            .extractMany(page, this.selectors.programme)
-            .then((programme) => programme.map((prog) => prog.trim())),
-          super
-            .extractText(page, this.selectors.duration)
-            .then((duration) => duration.split(" ")[0]),
-          super.extractText(page, this.selectors.pace).then((pace) => {
-            pace = pace.split(" ")[0];
-            if (pace.includes("–")) {
-              return pace.split("–")[1];
-            }
-            return pace;
-          }),
-        ]);
-      const { orga, languages } = await this.extractLanguagesAndOrga(page);
-      return {
-        title,
-        platform: this.platform,
-        url,
-        orga,
-        brief,
-        programme,
-        duration: pace * duration + ":00",
-        animateur,
-        languages,
-      };
-    } catch (error) {
-      console.error("Error scraping course data:", error);
-      return null;
-    } finally {
-      if (browser) {
-        await browser.close();
-      }
-    }
-  }
+	/**
+	 * Extract the languages and organization from the data
+	 * @param {object} page - The Puppeteer page object
+	 * @returns {object} - The extracted languages and organization
+	 * @memberof EDX
+	 * @method
+	 * @async
+	 */
+	async extractLanguagesAndOrga(page) {
+		try {
+			let elements = await page.$$(
+				'xpath///div[@class="course-about desktop course-info-content"]/div[4]/div//ul/li'
+			);
+
+			let data = {};
+			data["languages"] = [];
+			for (let element of elements) {
+				let value = await element.evaluate((el) => el.textContent);
+				if (
+					value.includes("Languages:") ||
+					value.includes("Language:")
+				) {
+					if (
+						value.split(": ")[1].includes("English") ||
+						value.split(": ")[1].includes("english")
+					) {
+						data["languages"].push("english");
+					}
+					if (
+						value.split(": ")[1].includes("French") ||
+						value.split(": ")[1].includes("french")
+					) {
+						data["languages"].push("french");
+					}
+					if (
+						value.split(": ")[1].includes("Arabic") ||
+						value.split(": ")[1].includes("arabic")
+					) {
+						data["languages"].push("arabic");
+					}
+				}
+				if (value.includes("Institution:")) {
+					data["orga"] = value.split(": ")[1];
+				}
+			}
+			return data;
+		} catch (error) {
+			console.error("Error scraping course data:", error);
+			return null;
+		}
+	}
+
+	/**
+	 * Extract the programme from the course page
+	 * @param {object} page - The Puppeteer page
+	 * @returns {array} - The programme of the course
+	 * @memberof EDX
+	 * @method
+	 * @async
+	 */
+	async extractProgramme(page) {
+		let programme = await super
+			.extractMany(page, this.selectors.programme)
+			.then((programme) => programme.map((prog) => prog.trim()));
+		if (programme.length === 0) {
+			programme = await super
+				.extractMany(page, this.selectors.altProgramme)
+				.then((programme) =>
+					programme[0].split("\n").map((prog) => prog.trim())
+				)
+				.catch(async () => {
+					return await super
+						.extractMany(page, this.selectors.altProgramme2)
+						.then((programme) => programme);
+				});
+		}
+		return programme;
+	}
+
+	/**
+	 * Scrape the data from the URL
+	 * @param {string} url - The URL to scrape
+	 * @returns {object} - The scraped data
+	 * @memberof EDX
+	 * @method
+	 * @async
+	 */
+	async scrape(url) {
+		try {
+			if (!(await super.checkURLExists(url))) {
+				console.error("URL '" + url + "' does not exist");
+				return;
+			}
+			var { browser, page } = await super.launchBrowser(url);
+
+			this.checkType(url);
+			if (this.type === "certificate") {
+				await page.click(
+					'xpath///main[@id="main-content"]/div[3]/div/div[1]/div[2]/div[1]/div/div[1]'
+				);
+				var orga = await super.extractText(page, this.selectors.orga);
+			} else {
+				var { languages, orga } = await this.extractLanguagesAndOrga(
+					page
+				);
+			}
+			const [title, brief, programme, duration, pace, animateur] =
+				await Promise.all([
+					await super.extractText(page, this.selectors.name),
+					this.type === "certificate"
+						? await super
+								.extractMany(page, this.selectors.brief)
+								.then((brief) =>
+									brief.map((el) => el.trim()).join(" ")
+								)
+						: await super
+								.extractMany(page, this.selectors.brief)
+								.then((brief) =>
+									brief.join(" ").trim().replace(/\n/g, "")
+								),
+					await this.extractProgramme(page),
+					await super
+						.extractText(page, this.selectors.duration)
+						.then((duration) => duration.split(" ")[0]),
+					this.type === "certificate"
+						? await super
+								.extractText(page, this.selectors.pace)
+								.then((pace) => {
+									pace = pace.split(" ")[2];
+									return pace;
+								})
+						: super
+								.extractText(page, this.selectors.pace)
+								.then((pace) => {
+									pace = pace.split(" ")[0];
+									if (pace.includes("–")) {
+										return pace.split("–")[1];
+									}
+									return pace;
+								}),
+					this.type === "certificate"
+						? await super.extractMany(
+								page,
+								this.selectors.animateur
+						  )
+						: super.extractManyWithMutation(
+								page,
+								this.selectors.animateur
+						  ),
+				]);
+
+			return {
+				title,
+				platform: this.platform,
+				url,
+				orga,
+				brief,
+				programme,
+				duration:
+					new String(pace * duration).length === 1
+						? `0${pace * duration}:00`
+						: `${pace * duration}:00`,
+				animateur,
+				languages: languages ? languages : this.detectLanguage(brief),
+			};
+		} catch (error) {
+			console.error("Error scraping course data:", error);
+			return null;
+		} finally {
+			if (browser) {
+				await browser.close();
+			}
+		}
+	}
 }
 
 module.exports = EDX;
--- sandbox.js
+++ sandbox.js
@@ -1,105 +1,306 @@
+const langdetect = require("langdetect");
 const Scraper = require("./Scraper");
+/**
+ * EDX scraper
+ * @extends Scraper
+ * @class
+ */
+class EDX extends Scraper {
+	/**
+	 * Create an EDX scraper
+	 * @constructor
+	 * @memberof EDX
+	 * @method
+	 */
+	constructor() {
+		super("EDX");
+		this.selectors = {
+			name: '//div[@class="course-about desktop course-info-content"]//div[1]/h1',
+			orga: '//div[@class="course-about desktop course-info-content"]//div[1]/ul/li[1]/a',
+			brief: '//div[@class="mt-2 lead-sm html-data"]',
+			programme: '//div[@class="mt-2 html-data"]/ul/li',
+			altProgramme: '//div[@class="mt-2 html-data"]/p[1]',
+			duration:
+				'//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[1]',
+			pace: '//div[@class="course-about desktop course-info-content"]/div[2]/div/div[1]/div/div/div[1]/div/div[2]',
+			animateur:
+				'//div[@class="instructor-card px-4 py-3.5 rounded"]/div/h3',
+			languages:
+				'//div[@class="course-about desktop course-info-content"]/div[4]/div/div[2]/div/div/div[2]/ul/li[1]',
+		};
+		this.type = "course";
+		// this.selectors.certificate = {
+		// 	name: '//div[@class="title"]',
+		// 	orga: '//div[@class="institution"]',
+		// 	brief: '//div[@class="overview-info"]/p',
+		// 	programme: '//li[@class="bullet-point mb-2"]',
+		// 	duration:
+		// 		'//div[@class="program-stat"][3]/div[@class="details"]/div[@class="main"]',
+		// 	pace: '//div[@class="program-stat"][3]/div[@class="details"]/div[@class="secondary"]',
+		// 	animateur: '//a[@class="name font-weight-bold"]',
+		// 	languages: "", //* detect from the brief
+		// };
+	}
+	checkType(url) {
+		if (url.includes("certificates")) {
+			this.type = "certificate";
+		}
+		this.switchSelectors();
+	}
+	switchSelectors() {
+		if (this.type === "certificate") {
+			this.selectors.name = '//div[@class="title"]';
+			this.selectors.orga = '//div[@class="institution"]';
+			this.selectors.brief = '//div[@class="overview-info"]/p';
+			this.selectors.programme = '//li[@class="bullet-point mb-2"]';
+			this.selectors.duration =
+				'//div[@class="program-stat"][3]/div[@class="details"]/div[@class="main"]';
+			this.selectors.pace =
+				'//div[@class="program-stat"][3]/div[@class="details"]/div[@class="secondary"]';
+			this.selectors.animateur = '//a[@class="name font-weight-bold"]';
+		}
+	}
 
-class ClassCentral extends Scraper {
-  constructor() {
-    super("ClassCentral");
-    this.selectors = {
-      //      name: '//main[@*[starts-with(name(), "data-lookup-course")]]',
-      //    orga: '//div[@class="provider"]/a',
-      brief:
-        '//div[@class="truncatable-area is-truncated wysiwyg text-1 line-wide"][1]',
+	detectLanguage(text) {
+		if (langdetect.detect(text)[0].lang === "fr") {
+			return ["french"];
+		}
+		if (langdetect.detect(text)[0].lang === "en") {
+			return ["english"];
+		}
+		if (langdetect.detect(text)[0].lang === "ar") {
+			return ["arabic"];
+		}
+		return [];
+	}
+	/**
+	 * Extract the languages and organization from the data
+	 * @param {object} page - The Puppeteer page object
+	 * @returns {object} - The extracted languages and organization
+	 * @memberof EDX
+	 * @method
+	 * @async
+	 */
+	async extractLanguagesAndOrga(page) {
+		try {
+			let elements = await page.$$(
+				'xpath///div[@class="course-about desktop course-info-content"]/div[4]/div//ul/li'
+			);
 
-      programme:
-        '//div[@class="truncatable-area is-truncated wysiwyg text-1 line-wide"]/ul/li',
-      duration: '//div[@id="details-contents"]/ul/li[5]/div[2]/span',
-      animateur: '//*[@id="page-course"]/div[1]/main//div/section[3]/div[1]/p',
-      languages: '//div[@id="details-contents"]/ul/li[7]/div[2]/span',
-    };
-  }
+			let data = {};
+			data["languages"] = [];
+			for (let element of elements) {
+				let value = await element.evaluate((el) => el.textContent);
+				if (
+					value.includes("Languages:") ||
+					value.includes("Language:")
+				) {
+					if (
+						value.split(": ")[1].includes("English") ||
+						value.split(": ")[1].includes("english")
+					) {
+						data["languages"].push("english");
+					}
+					if (
+						value.split(": ")[1].includes("French") ||
+						value.split(": ")[1].includes("french")
+					) {
+						data["languages"].push("french");
+					}
+					if (
+						value.split(": ")[1].includes("Arabic") ||
+						value.split(": ")[1].includes("arabic")
+					) {
+						data["languages"].push("arabic");
+					}
+				}
+				if (value.includes("Institution:")) {
+					data["orga"] = value.split(": ")[1];
+				}
+			}
+			return data;
+		} catch (error) {
+			console.error("Error scraping course data:", error);
+			return null;
+		}
+	}
 
-  async extractLanguages(page) {
-    const languages = [];
-    let langs = await super
-      .extractText(page, this.selectors.languages)
-      .then((langs) => langs.split(", ").map((lang) => lang.trim()))
-      .catch(() => ["English"]);
-    if (langs.includes("English")) {
-      languages.push("english");
-    }
-    if (langs.includes("French")) {
-      languages.push("french");
-    }
-    if (langs.includes("Arabic")) {
-      languages.push("arabic");
-    }
-    return languages;
-  }
-  async extractDuration(page) {
-    const duration = await super
-      .extractText(page, this.selectors.duration)
-      .then((duration) => {
-        duration = duration.split("")[0].trim();
-        return duration.length === 1 ? `0${duration}:00` : `${duration}:00`;
-      });
-    return duration;
-  }
-  async scrape(url) {
-    try {
-      var { browser, page } = await super.launchBrowser(url);
+	async extractProgramme(page) {
+		let programme = await super
+			.extractMany(page, this.selectors.programme)
+			.then((programme) => programme.map((prog) => prog.trim()));
+		if (programme.length === 0) {
+			programme = await super
+				.extractMany(page, this.selectors.altProgramme)
+				.then((programme) =>
+					programme[0].split("\n").map((prog) => prog.trim())
+				);
+		}
+		return programme;
+	}
+	async extractTextPostsMutation(page, xpath) {
+		return await page.evaluate(async (xpath) => {
+			const waitForElements = (xpath) => {
+				return new Promise((resolve) => {
+					const observer = new MutationObserver((mutations) => {
+						const elements = document.evaluate(
+							xpath,
+							document,
+							null,
+							XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+							null
+						);
+						const results = [];
+						for (let i = 0; i < elements.snapshotLength; i++) {
+							results.push(
+								elements.snapshotItem(i).textContent.trim()
+							);
+						}
+						if (results.length > 0) {
+							observer.disconnect();
+							resolve(results);
+						}
+					});
+					observer.observe(document, {
+						childList: true,
+						subtree: true,
+					});
+				});
+			};
 
-      const all = await super
-        .extractAttribute(
-          page,
-          '//*[@id="btnProviderCoursePage"]',
-          "data-track-props"
-        )
-        .then((all) => JSON.parse(all));
+			return await waitForElements(xpath);
+		}, xpath);
+	}
+	async extractTextPostMutation(page, xpath) {
+		return await page.evaluate(async (xpath) => {
+			const waitForElement = (xpath) => {
+				return new Promise((resolve) => {
+					const observer = new MutationObserver((mutations) => {
+						const element = document.evaluate(
+							xpath,
+							document,
+							null,
+							XPathResult.FIRST_ORDERED_NODE_TYPE,
+							null
+						).singleNodeValue;
+						if (element) {
+							observer.disconnect();
+							resolve(element);
+						}
+					});
+					observer.observe(document, {
+						childList: true,
+						subtree: true,
+					});
+				});
+			};
 
-      const title = all["course_name"];
-      const orga = all["course_institution"];
-      const languages = all["course_language"];
-      const [brief, programme, duration, animateur] = await Promise.all([
-        super
-          .extractText(page, this.selectors.brief)
-          .then((brief) => brief.replace(/(\r\n|\n|\r)/gm, "").trim()),
-        super.extractMany(page, this.selectors.programme),
-        this.extractDuration(page),
-        super.extractMany(page, this.selectors.animateur),
-      ]);
+			const element = await waitForElement(xpath);
+			return element ? element.textContent.trim() : null;
+		}, xpath);
+	}
+	/**
+	 * Scrape the data from the URL
+	 * @param {string} url - The URL to scrape
+	 * @returns {object} - The scraped data
+	 * @memberof EDX
+	 * @method
+	 * @async
+	 */
+	async scrape(url) {
+		try {
+			if (!(await super.checkURLExists(url))) {
+				console.error("URL '" + url + "' does not exist");
+				return;
+			}
+			var { browser, page } = await super.launchBrowser(url);
 
-      //   const languages = await this.extractLanguages(page);
-      //   const brief = await super
-      //     .extractText(page, this.selectors.brief)
-      //     .then((brief) => brief.replace(/(\r\n|\n|\r)/gm, "").trim());
-      //   const programme = await super.extractMany(page, this.selectors.programme);
-      //   const duration = await this.extractDuration(page);
-      //   const animateur = await super.extractMany(page, this.selectors.animateur);
-      console.log({
-        title,
-        platform: this.platform,
-        url,
-        orga,
-        brief,
-        programme,
-        duration,
-        animateur,
-        languages,
-      });
-    } catch (error) {
-      console.error("Error scraping course data:", error);
-      return null;
-    } finally {
-      if (browser) {
-        await browser.close();
-      }
-    }
-  }
+			this.checkType(url);
+			if (this.type === "certificate") {
+				await page.click(
+					'xpath///main[@id="main-content"]/div[3]/div/div[1]/div[2]/div[1]/div/div[1]'
+				);
+				var orga = await super.extractText(page, this.selectors.orga);
+			} else {
+				var { languages, orga } = await this.extractLanguagesAndOrga(
+					page
+				);
+			}
+			const [title, brief, programme, duration, pace, animateur] =
+				await Promise.all([
+					await super.extractText(page, this.selectors.name),
+					this.type === "certificate"
+						? await super
+								.extractMany(page, this.selectors.brief)
+								.then((brief) =>
+									brief.map((el) => el.trim()).join(" ")
+								)
+						: await super
+								.extractMany(page, this.selectors.brief)
+								.then((brief) =>
+									brief.join(" ").trim().replace(/\n/g, "")
+								),
+					await this.extractProgramme(page),
+					await super
+						.extractText(page, this.selectors.duration)
+						.then((duration) => duration.split(" ")[0]),
+					this.type === "certificate"
+						? await super
+								.extractText(page, this.selectors.pace)
+								.then((pace) => {
+									pace = pace.split(" ")[2];
+									return pace;
+								})
+						: super
+								.extractText(page, this.selectors.pace)
+								.then((pace) => {
+									pace = pace.split(" ")[0];
+									if (pace.includes("–")) {
+										return pace.split("–")[1];
+									}
+									return pace;
+								}),
+					this.type === "certificate"
+						? await super.extractMany(
+								page,
+								this.selectors.animateur
+						  )
+						: super.extractManyWithMutation(
+								page,
+								this.selectors.animateur
+						  ),
+				]);
+
+			console.log({
+				title,
+				platform: this.platform,
+				url,
+				orga,
+				brief,
+				programme,
+				duration:
+					(pace * duration).length === 1
+						? `0${pace * duration}:00`
+						: `${pace * duration}:00`,
+				animateur,
+				languages: languages ? languages : this.detectLanguage(brief),
+			});
+		} catch (error) {
+			console.error("Error scraping course data:", error);
+			return null;
+		} finally {
+			if (browser) {
+				await browser.close();
+			}
+		}
+	}
 }
-let scraper = new ClassCentral();
-// scraper.scrape(
-//   "https://classcentral.com/course/independent-data-analysis-with-python-204189"
-// );
-scraper.scrape(
-  "https://www.classcentral.com/course/google-analytics-for-beginners-98262"
+let edx = new EDX();
+edx.scrape(
+	"https://www.edx.org/learn/python/harvard-university-cs50-s-introduction-to-programming-with-python"
+	// "https://www.edx.org/certificates/professional-certificate/ubcx-online-teaching-for-educators-development-and-delivery"
+	// "https://www.edx.org/certificates/professional-certificate/armeducationx-embedded-systems-essentials"
+	// "https://www.edx.org/learn/embedded-systems/the-university-of-texas-at-austin-embedded-systems-shape-the-world-microcontroller-input-output"
+	// "https://www.edx.org/learn/coding/universita-degli-studi-di-napoli-federico-ii-coding-a-scuola-con-software-libero"
 );
-// scraper.scrape("https://www.classcentral.com/course/journalism-6009");
+// #main-content > div.gradient-wrapper.program-body.container-mw-lg.container-fluid > div > div:nth-child(1) > div:nth-child(2) > div.col-lg-8.mb-4 > div.pgn_collapsible.p-2.collapsible-card.is-open > div.pgn-transition-replace-group.position-relative > div > div > div > p:nth-child(6)
